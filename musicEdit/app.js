df434987e9d923da1dfaa5f976d52ffd40bb1708
Werty <limhm1396@naver.com>
2020-08-10 23:07:31 +0900

    null

--- musicEdit/app.js
+++ musicEdit/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const musicEditRouter = require('./routes/music_edit');
 
 const models = require("./models/index.js");
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.usee('/music_edit', musicEditRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
