98c8879f089eaaf5e66f23ad0eb345e44576f0f4
werty <limhm1396@naver.com>
2020-08-10 10:49:31 +0000

    node-id3

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var ffmetadata = require("ffmetadata");
- 
-// Read song.mp3 metadata
-ffmetadata.read("../music/01 Bach_ Lute Suite In E Minor, BWV.mp3", function(err, data) {
-    if (err) console.error("Error reading metadata", err);
-    else console.log(data);
-});
- 
