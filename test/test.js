2533199e86fe0fd19726d5b25596356c5caa91a2
Werty <limhm1396@naver.com>
2020-08-10 16:00:55 +0900

    test success

--- test/test.js
+++ test/test.js
@@ -1,10 +1,13 @@
 const mm = require('music-metadata');
 const util = require('util');
-
+ 
 mm.parseFile('./music/02 Bach_ Lute Suite In E Minor, BWV.mp3')
-    .than(metadata => {
-        console.log(util.inspect(metadata, { showHidden: false, depth: null }));
-    })
-    .catch(err => {
-        console.error(err.message);
-    });
+  .then( metadata => {
+    console.log('metadata : ', util.inspect(metadata));
+    console.log('metadata-title : ', util.inspect(metadata.common.title));
+    console.log('metadata-album : ', util.inspect(metadata.common.album));
+    console.log('metadata-artist : ', util.inspect(metadata.common.artist));
+  })
+  .catch( err => {
+    console.error(err.message);
+  });
