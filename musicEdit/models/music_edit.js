67a3d878794bb0bbaa55ff0f16136a3b9f1d0cf8
Werty <limhm1396@naver.com>
2020-08-11 15:39:35 +0900

    x

--- musicEdit/models/music_edit.js
+++ musicEdit/models/music_edit.js
@@ -1,40 +1,33 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class music_edit extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  music_edit.init({
-    fileName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    albumName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    artistName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    filePath: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
-    sequelize,
-    modelName: 'music_edit',
-    charset: 'utf8',
-    collate: 'utf8_general_ci',
-  });
-  return music_edit;
-};
+
+const Sequelize = require('sequelize');
+
+module.exports = class music_edit extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      fileName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      albumName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      artistName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      filePath: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    }, {
+      sequelize,
+      modelName: 'music_edit',
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {}
+}
