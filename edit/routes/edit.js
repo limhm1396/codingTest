ada28afaba2d96d488ef93604d510301781a292d
Werty <limhm1396@naver.com>
2020-08-11 20:33:47 +0900

    x

--- edit/routes/edit.js
+++ edit/routes/edit.js
@@ -3,8 +3,17 @@ const router = express.Router();
 const multer = require("multer");
 
 //뷰 페이지 경로
-router.get('/musicList', function(req, res, next) {
-    res.render('edit/musicList');
+router.get('/musicList', async function(req, res, next) {
+    try {
+        const result = await models.post.findAll({
+            attributes: ['id', 'fileName', 'albumName', 'artistName'],
+        })
+        res.render("edit/musicList", {
+            posts: result
+        });
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 module.exports = router;
