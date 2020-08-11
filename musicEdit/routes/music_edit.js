c646255c64d49d8d615ecec7a6c9783dcd762164
Werty <limhm1396@naver.com>
2020-08-11 14:45:08 +0900

    null

--- musicEdit/routes/music_edit.js
+++ musicEdit/routes/music_edit.js
@@ -3,6 +3,10 @@ const MusicEdit = require('../models/music_edit');
 
 const router = express.Router();
 
+router.get('/musicEdit', function (req, rex, next) {
+    res.render('/music/musicList');
+});
+
 router.get('/:id/lists', async (req, res, next) => {
     try {
         const list = await MusicEdit.findAll({
