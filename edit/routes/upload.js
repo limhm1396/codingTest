fe18eab43b239202ad55d90e3e2a6131511090f5
Werty <limhm1396@naver.com>
2020-08-11 19:13:47 +0900

    x

--- edit/routes/upload.js
+++ edit/routes/upload.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const multer = require("multer");
+const path = require("path");
 
-// 1. multer 미들웨어 등록
+let storage = multer.diskStorage({
+    destination: function(req, file ,callback){
+        callback(null, "upload/")
+    },
+    filename: function(req, file, callback){
+        let extension = path.extname(file.originalname);
+        let basename = path.basename(file.originalname, extension);
+        callback(null, basename + "."+ extension);
+    }
+});
+
+//multer 미들웨어 등록
 let upload = multer({
-    dest: "upload/"
+    storage: storage
 })
 
-// 2. 파일 업로드 처리
+//파일 업로드 처리
 router.post('/create', upload.single("imgFile"), function(req, res, next) {
-    // 3. 파일 객체
+    //파일 객체
     let file = req.file
 
-    // 4. 파일 정보
+    //파일 정보
     let result = {
         originalName : file.originalname,
         size : file.size,
