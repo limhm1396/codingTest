beebf16a1184c64d548dcdc247ae838d6e53957c
werty <limhm1396@naver.com>
2020-08-11 04:10:25 +0000

    null

--- musicEdit/migrations/create-music-edit.js
+++ musicEdit/migrations/create-music-edit.js
@@ -2,24 +2,35 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('music_edits', {
-      fileName: {
-        type: Sequelize.STRING,
+      id: {
         allowNull: false,
+        autoIncrement: true,
         primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      fileName: {
+        type: Sequelize.STRING
       },
       albumName: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
       artistName: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        primaryKey: true,
+        type: Sequelize.STRING
       },
       filePath: {
-        type: Sequelize.STRING,
-        allowNull: false,
+        type: Sequelize.STRING
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
     });
   },
   down: async (queryInterface, Sequelize) => {
