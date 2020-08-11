b6ded1277435100e3a8c178dfff90f7829e1b38c
Werty <limhm1396@naver.com>
2020-08-11 19:02:03 +0900

    x

--- edit/app.js
+++ edit/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const editRouter = require('./routes/edit');
+const fileuploadRouter = require('./routes/upload');
 const models = require('./models/index.js');
 
 async function dbConnect () {
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/edit', editRouter);
+app.use('/upload', fileUploadRouter);
 app.use('/upload', express.static('uploads'));
 
 // catch 404 and forward to error handler
