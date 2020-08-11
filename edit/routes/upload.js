becdd89243d60283b642eb01cbcf0069b0586069
Werty <limhm1396@naver.com>
2020-08-11 19:40:12 +0900

    x

--- edit/routes/upload.js
+++ edit/routes/upload.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const models = require('../models');
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
+const NodeID3 = require('node-id3');
 
 let storage = multer.diskStorage({
     destination: function(req, file ,callback){
@@ -14,10 +16,34 @@ let storage = multer.diskStorage({
     }
 });
 
+async function dbInsert (tags, storage) {
+    try {
+        await models.edit.create({
+            fileName: tags.title,
+            albumName: tags.album,
+            artistName: tags.artist,
+            filePath: storage,
+        })
+        console.log('음원 추가 완료');
+    } catch (err) {
+        console.log('음원 추가 실패');
+        console.error(err);
+    }
+}
+
 //multer 미들웨어 등록
-let upload = multer({
-    storage: storage
-})
+let upload = async function() {
+    try {
+        await multer({
+            storage: storage
+        });
+        const file = storage;
+        let tags = await NodeID3.read(file);
+        dbInsert(tags, storage);
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 //파일 업로드 처리
 router.post('/create', upload.single("imgFile"), function(req, res, next) {
@@ -30,7 +56,7 @@ router.post('/create', upload.single("imgFile"), function(req, res, next) {
         size : file.size,
     }
 
-    res.json(result);
+    res.redirect(200, '/edit/musicList');
 });
 
 module.exports = router;
