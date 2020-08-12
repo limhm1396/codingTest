14c4f290515315c9235b90e625c18cd359696d6d
Werty <limhm1396@naver.com>
2020-08-12 13:41:37 +0900

    x

--- edit/app.js
+++ edit/app.js
@@ -26,6 +26,8 @@ dbConnect();
 
 var app = express();
 
+app.use(methodOverride('_method'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -35,7 +37,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
