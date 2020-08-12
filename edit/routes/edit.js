dfeba86c56f1263a5d7b6d621175fed2000b0569
Werty <limhm1396@naver.com>
2020-08-12 18:46:54 +0900

    x

--- edit/routes/edit.js
+++ edit/routes/edit.js
@@ -101,6 +101,7 @@ router.delete('/musicList/:id', async function(req, res, next) {
     })
 
     console.log('filePath : ', filePath.filePath);
+    console.log('__dirName : ', __dirname);
     fs.unlinkSync('../' + filePath.filePath);
 
     res.redirect(200,'/edit/musicList');
