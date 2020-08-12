dc996313f4452800ea90a0eef4da35bcfea703ae
Werty <limhm1396@naver.com>
2020-08-12 15:16:47 +0900

    x

--- edit/routes/edit.js
+++ edit/routes/edit.js
@@ -19,6 +19,21 @@ router.get('/musicList', async function(req, res, next) {
     }
 });
 
+router.get('/ascList', async function(req, res, next) {
+    try {
+        const result = await models.edit.findAll({
+            attributes: ['id', 'fileName', 'albumName', 'artistName'],
+        }, {
+            order: ['fileName', "ASC"],
+        })
+        res.render("edit/musicList", {
+            posts: result
+        });
+    } catch (err) {
+        console.error(err);
+    }
+});
+
 //수정
 router.get('/musicEdit/:id', async function(req, res, next) {
     try {
@@ -90,7 +105,7 @@ router.delete('/musicList/:id', async function(req, res, next) {
         console.error(err);
     });
 
-    res.redirect('/musicList');
+    res.redirect(200,'/edit/musicList');
     } catch (err) {
       console.error(err);
     }
