06b1d17483905e6daeaff82d44d58c68c5b931e8
Werty <limhm1396@naver.com>
2020-08-12 13:40:37 +0900

    x

--- edit/app.js
+++ edit/app.js
@@ -35,13 +35,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/edit', editRouter);
 app.use('/upload', fileUploadRouter);
 app.use('/upload', express.static('uploads'));
-app.use(methodOverride('_method'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
